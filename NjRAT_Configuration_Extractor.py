38868d82d6ab8b32c8f9725b9d42043032970002
Spider0x <74156139+amr-git-dot@users.noreply.github.com>
2023-03-29 14:08:21 +0200

    Update NjRAT_Configuration_Extractor.py

--- NjRAT_Configuration_Extractor.py
+++ NjRAT_Configuration_Extractor.py
@@ -1,31 +1,38 @@
-import re
 import sys
+import re
 
-pattern = b'\x57\x52\x4B\x00\x6D\x61\x69\x6E'
-with open(sys.argv[1], 'rb') as f:
-    data = f.read()
-    match = re.search(pattern, data)
-    if match:
-        print(f"Pattern found at byte offset {match.start()}")
-        config = [] 
-        offset = match.start()
-        f.seek(offset+ 11)
-        unicode_data = f.read(315)
-        
-        for data in unicode_data :
-            data = data.to_bytes(1, byteorder="little")
-            if data > b'\x00' and data < b'\x2E' and data != b'1B' and data != b'27':
-                config.append("&")
-            elif data < b'\x7F' and data > b'\x20':
-                try :
-                    config.append(data.decode("utf-8"))
-                except:
-                    pass
-        config = ''.join(config)            
-        config = config.split("&")
-        for i in range(0,12):
-            print(config[i])
+if len(sys.argv) < 2:
+    print("Usage: python script.py <filename>")
+    sys.exit(1)
 
-        
-    else:
-        print("Configuration offset not found")
+filename = sys.argv[1]
+
+with open(filename, "rb") as f:
+    cont = f.read()
+
+match = re.search(b"\x47\x65\x74\x57\x69\x6E\x64\x6F\x77\x54\x65\x78\x74.*\x00\x00", cont)
+
+print(f"match found at {match.start()}")
+
+nullmatch = re.search(b'\x00\x00', cont[match.start():])
+
+print(f"null match at {nullmatch.start()+ match.start()}")
+unicode_offset = nullmatch.start()+ match.start()
+unicode_data = cont[unicode_offset:300+unicode_offset]
+
+config = []
+for data in unicode_data :
+    data = data.to_bytes(1, byteorder="little")
+    
+    if data > b'\x00' and data < b'\x2E' and data != b'1B' and data != b'27':
+        config.append("&")
+    elif data < b'\x7F' and data > b'\x20':
+        try :
+            config.append(data.decode("utf-8"))
+        except:
+            pass
+
+config = ''.join(config)            
+config = config.split("&")
+for i in range(0,len(config)):
+    print(config[i])
